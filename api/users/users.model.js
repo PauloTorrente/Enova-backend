f26f0c41c0ec21609ecd91f98b3f246f56bdc237
paulotorrente <paulotorrentebr@gmail.com>
2025-02-13 15:33:30 +0100

    respondents removed

--- api/users/users.model.js
+++ api/users/users.model.js
@@ -104,16 +104,6 @@ const User = sequelize.define(
       allowNull: true, 
       field: 'education_level',
     },
-    respondentId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'respondents',
-        key: 'id',
-      },
-      onDelete: 'SET NULL',
-      field: 'respondent_id',
-    },
     walletBalance: {
       type: DataTypes.FLOAT,
       defaultValue: 0, 
