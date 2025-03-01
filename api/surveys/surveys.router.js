0c81d254ca38457d822c419872e60a68eb5d573a
paulotorrente <paulotorrentebr@gmail.com>
2025-03-01 14:48:15 +0100

    get respond added

--- api/surveys/surveys.router.js
+++ api/surveys/surveys.router.js
@@ -12,8 +12,8 @@ router.delete('/:id', authenticateAdmin, surveysController.deleteSurvey); // Del
 // Public routes
 router.get('/active', surveysController.getActiveSurveys); // Get active surveys
 
-// Route to respond to a survey by token
-router.post('/respond', authenticateUser, surveysController.respondToSurveyByToken); // Respond to a survey by token (authentication required)
+// Route to get a survey by access token
+router.get('/respond', authenticateUser, surveysController.getSurveyByAccessToken); // Get survey by access token
 
 // Route to respond to a survey by token
 router.post('/respond', authenticateUser, surveysController.respondToSurveyByToken); // Respond to a survey by token (authentication required)
