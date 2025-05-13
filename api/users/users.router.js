bc65c9c79bfde37c1cc1e4b8bd75ccad134ace83
paulotorrente <paulotorrentebr@gmail.com>
2025-05-14 00:08:14 +0200

    /me added to identify the user by JWT

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -8,8 +8,25 @@ import {
   getWalletBalance 
 } from './users.controller.js';
 import { authenticateUser, authenticateAdmin } from '../../middlewares/auth.middleware.js'; 
+import User from './users.model.js';
 
 const router = express.Router();
+
+router.get('/me', authenticateUser, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+    const { password, ...safeUser } = user.toJSON();
+    res.status(200).json(safeUser);
+
+  } catch (error) {
+    console.error('Erro ao buscar perfil:', error);
+    res.status(500).json({ message: 'Erro interno no servidor' });
+  }
+});
 
 // Routes for handling user actions
 router.get('/', getAllUsers); 
